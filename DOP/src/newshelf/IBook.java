bf5c8da116f71bca302a83fe4a913ce870f66ff7
asifmohammed786 <asifmohammed85113@gmail.com>
2024-10-27 16:30:24 +0530

    update

--- DOP/src/newshelf/IBook.java
+++ DOP/src/newshelf/IBook.java
@@ -1,5 +1,13 @@
 package newshelf;
 
-public interface IBook {
-    
+public sealed interface IBook permits Comic,Fiction,TextBook{}
+
+enum FictionType {
+	Comedy,
+	Tragedy,
+	GoK,DEFAULT
 }
+
+record Comic(String Title,int ageOfMainCharacter) implements IBook {}
+record Fiction(String name,FictionType type) implements IBook {}
+record TextBook(String subject) implements IBook {}
--- DOP/src/newshelf/NewSelection.java
+++ DOP/src/newshelf/NewSelection.java
@@ -12,16 +12,27 @@ public class NewSelection {
 	 */
 	public static String getAgeOrTitle(Object o) {
 		
-		if (o instanceof Book) {
-			
+		if (o instanceof Comic c) {
+			return ((Comic) o).Title();
 		}
+		if(o instanceof TextBook t){
+			return ((TextBook) o).subject();
+		}	
+		if(o instanceof Fiction f){
+			return ((Fiction) o).name();
+		}
+
 		return null;
 	}
 
 	public static void main(String[] args) {
 		
-		// TODO: Write a test code here and execute and text.
-		TextBook t = new TextBook();
+		TextBook tb1 = new TextBook("C programming");
+		Comic comic1 = new Comic("Batman",29);
+		Fiction fiction1 = new Fiction("Sorrow World",FictionType.Tragedy);
+		System.out.println(getAgeOrTitle(fiction1));
+		System.out.println(getAgeOrTitle(comic1));
+		System.out.println(getAgeOrTitle(tb1));
 		
 	}
 }
